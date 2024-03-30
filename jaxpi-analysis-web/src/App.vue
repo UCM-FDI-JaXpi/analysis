e76a119b94b68f704a4e2e366e32595f229d552d
miriamCR <miriamcabana96@gmail.com>
2024-03-30 23:55:59 +0100

    vue v3 instead of vue v2

--- jaxpi-analysis-web/src/App.vue
+++ jaxpi-analysis-web/src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
     <h1>Bar Chart</h1>
-    <BarChart />
+    <BarChart :data="jsonData" />
   </div>
 </template>
 
 <script>
 import BarChart from './components/BarChart.vue';
+import jsonData from './data/data.json';
 
 export default {
   name: 'App',
   components: {
     BarChart
+  },
+  data(){
+    return {
+      jsonData
+    };
   }
 }
 </script>
--- jaxpi-analysis-web/src/components/BarChart.vue
+++ jaxpi-analysis-web/src/components/BarChart.vue
@@ -1,52 +1,56 @@
 <template>
-    <div id="bar-chart"></div>
-  </template>
-  
-  <script>
-  import c3 from 'c3';
-  import 'c3/c3.css';
-  import jsonData from '../data/data.json'; // Import data from JSON file
-  
-  export default {
-    mounted() {
-      this.drawBarChart(jsonData); // Pass the JSON data to the drawBarChart method
-    },
-    methods: {
-      drawBarChart(data) {
-        const names = data.map(item => item.nombre);
-        const values = data.map(item => item.valor);
-  
-        c3.generate({
-          bindto: '#bar-chart',
-          data: {
-            columns: [
-              ['Player Score'].concat(values) // Add 'Value' as column header and player values
-            ],
-            type: 'bar'
+  <div id="bar-chart"></div>
+</template>
+
+<script>
+import c3 from 'c3';
+import 'c3/c3.css';
+
+export default {
+  props: {
+    data: {
+      type: Array,
+      required: true
+    }
+  },
+  mounted() {
+    this.drawBarChart(this.data);
+  },
+  methods: {
+    drawBarChart(data) {
+      const names = data.map(item => item.nombre);
+      const values = data.map(item => item.valor);
+
+      c3.generate({
+        bindto: '#bar-chart',
+        data: {
+          columns: [
+            ['Player Score'].concat(values)
+          ],
+          type: 'bar'
+        },
+        axis: {
+          x: {
+            type: 'category',
+            categories: names // X axis: name of the players
           },
-          axis: {
-            x: {
-              type: 'category',
-              categories: names // X axis: name of the players
-            },
-            y: {
-              label: {
-                text: 'Score',
-                position: 'outer-middle'
-              }
+          y: {
+            label: {
+              text: 'Score',
+              position: 'outer-middle'
             }
           }
-        });
-      }
-    },
-    unmounted() {
-      // Destruye el gráfico cuando el componente es desmontado para evitar fugas de memoria
-      if (this.chart) {
-        this.chart.destroy();
-      }
+        }
+      });
     }
+  },
+  unmounted() {
+    // Destruye el gráfico cuando el componente es desmontado para evitar fugas de memoria
+    if (this.chart)
+      this.chart.destroy();
   }
-  </script>
-  
-  <style>
-  </style>
+}
+</script>
+
+<style>
+</style>
