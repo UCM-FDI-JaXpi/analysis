1e9f8856a703aaf4f4da6b592b0d49fc4027d42d
marc057 <macolomb@ucm.es>
2024-04-14 22:56:25 +0200

    Add Login and Register views

---
+++ jaxpi-analysis-web/src/views/LoginView.vue
@@ -0,0 +1,31 @@
+<template>
+	<h1>Login</h1>
+
+	<form action="http://localhost:3000/login" method="POST">
+		<div>
+			<label for="email">Email</label>
+			<input type="text" id="email" name="email" required>
+		</div>
+		<div>
+			<label for="password">Password</label>
+			<input type="password" id="password" name="password" required>
+		</div>
+		<button type="submit">Login</button>
+	</form>
+	<a href="/register">Register</a>
+</template>
+
+<script setup>
+</script>
+
+<style>
+	h1 {
+		font-size: 25px;
+		color: #333;
+	}
+
+	.link {
+	display: block;
+	margin-bottom: 10px; /* Space between links */
+	}
+</style>
---
+++ jaxpi-analysis-web/src/views/RegisterView.vue
@@ -0,0 +1,48 @@
+<template>
+		<h1>Register</h1>
+
+<form action="http://localhost:3000/register" method="POST">
+	<div>
+		<label for="name">Name</label>
+		<input type="text" id="name" name="name" required>
+	</div>
+	<div>
+		<label for="email">Email</label>
+		<input type="text" id="email" name="email" required>
+	</div>
+	<div>
+		<label for="pwd">Password</label>
+		<input type="password" id="pwd" name="pwd" required>
+	</div>
+	<div>
+		<label for="rep_pwd">Repeat password</label>
+		<input type="password" id="rep_pwd" name="rep_pwd" required>
+	</div>
+	<div>
+        <label for="usr_type">I am a:</label>
+        <select name="usr_type" id="usr_type">
+            <option value="student">Student</option>
+            <option value="teacher">Teacher</option>
+            <option value="dev">Developer</option>
+        </select>
+    </div>
+	<button type="submit">Register</button>
+</form>
+<a href="/login">Login</a>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style>
+  h1 {
+    font-size: 25px;
+    color: #333;
+  }
+  
+  .link {
+    display: block;
+    margin-bottom: 10px; /* Space between links */
+  }
+  </style>
+  
