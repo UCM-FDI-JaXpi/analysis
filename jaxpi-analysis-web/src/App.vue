c48470e3bdbc844997e505794d65617e039aa5a9
miriamCR <miriamcabana96@gmail.com>
2024-04-16 19:58:04 +0200

    add sockets configuration

--- jaxpi-analysis-web/src/App.vue
+++ jaxpi-analysis-web/src/App.vue
@@ -1,10 +1,19 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <router-view :socket="socket"></router-view>
   </div>
 </template>
 
 <script setup>
+import { onBeforeUnmount } from 'vue';
+import io from "socket.io-client"
+
+const socket = io("http://localhost:3000");
+
+// Before the component is destroyed 
+onBeforeUnmount(() => {
+  socket.disconnect(); // Close the websocket connection
+});
 </script>
 
 <style>
