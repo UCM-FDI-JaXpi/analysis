3c4acbbd0ccf4aae4c76a0aa4d6f604a98abd1cd
miriamCR <miriamcabana96@gmail.com>
2024-07-26 03:46:31 +0200

    refactor: add submenu to the sidebar, delete student sidebar

--- jaxpi-analysis-web/src/components/SidebarMenu.vue
+++ jaxpi-analysis-web/src/components/SidebarMenu.vue
@@ -1,24 +1,32 @@
 <template>
-    <div v-if="userType === 'teacher' || userType === 'student'" class="sidebar"> <!--Dev no tiene sidebar-->
+    <div v-if="userType === 'teacher'" class="sidebar"> <!--Ni Dev ni Student tienen sidebar-->
         <template v-if="userType === 'teacher'">
-            <router-link to="/teacher/classes">Classes</router-link>
-            <router-link to="/teacher/students">Students</router-link>
-            <router-link to="/teacher/games">Games</router-link>
-        </template>
-        <template v-else-if="userType === 'student'">
-            <router-link to="/student/games">Games stats</router-link>
-            <router-link to="/student/classes">Classes stats</router-link>
-            <router-link to="/student/profile">Profile</router-link>
+            <router-link to="/teacher/sessions" class="menu-link">Sessions</router-link>
+            <div>
+                <div class="menu-item" @click="toggleTeacherClassesSubmenu">
+                    Classes
+                    <span>{{ isTeacherClassesSubmenuOpen ? '-' : '+' }}</span>
+                </div>
+                <div v-if="isTeacherClassesSubmenuOpen" class="submenu">
+                    <router-link to="/teacher/classes/classA" class="submenu-link">A</router-link>
+                    <router-link to="/teacher/classes/classB" class="submenu-link">B</router-link>
+                </div>
+            </div>
         </template>
     </div>
 </template>
 
 <script setup>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useAuthStore } from '@/stores/authStore';
 
 const authStore = useAuthStore();
 const userType = computed(() => authStore.userType);
+const isTeacherClassesSubmenuOpen = ref(false);
+
+const toggleTeacherClassesSubmenu = () => {
+    isTeacherClassesSubmenuOpen.value = !isTeacherClassesSubmenuOpen.value;
+};
 </script>
 
 <style scoped>
@@ -30,8 +38,27 @@ const userType = computed(() => authStore.userType);
     flex-direction: column;
 }
 
-.sidebar a {    /* router-link */
-    margin: 10px 0;
+.menu-link {
+    margin: 7px 0;
+    text-decoration: none;
+    color: #333;
+}
+
+.menu-item { /* To menu options that have submenus */
+    margin: 7px 0;
+    cursor: pointer;
+    color: #333;
+    display: flex;
+    justify-content: space-between;
+}
+
+.submenu {
+    padding-left: 20px;
+}
+
+.submenu-link {
+    display: block;
+    margin: 5px 0;
     text-decoration: none;
     color: #333;
 }
